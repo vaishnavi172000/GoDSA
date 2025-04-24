3bc5a296ee627d003c547b7434b9386834d360b2
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-04-24 13:38:42 +0530

    remove duplicates

--- Array/main.go
+++ Array/main.go
@@ -22,6 +22,32 @@ func FindSecondlargest(arr []int) int {
 	return secondMax
 }
 
+// Given an integer array nums sorted in non-decreasing order,
+// remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+func removeDuplicates(arr []int) int {
+	n := len(arr)
+	cnt := 0
+	j := 0
+	for i := 0; i < n; i++ {
+		arr[j] = arr[i]
+		for i != n-1 && arr[i] == arr[i+1] {
+			i++
+		}
+		j++
+		cnt++
+	}
+	return cnt
+
+}
+
 func main() {
 	var n int
 	fmt.Println("Enter the size of the array")
