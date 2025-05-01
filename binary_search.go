8e6dd0d9d18a814303b58398bd8e168b034380ad
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-01 06:56:06 +0530

    First and Last occurance of element in the sorted array

--- binary_search.go
+++ binary_search.go
@@ -92,3 +92,74 @@ func maxbouq(bloom []int, day int, k int) int {
 	}
 	return bouquets
 }
+
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/?envType=problem-list-v2&envId=array
+// Given an array of integers nums sorted in non-decreasing order,
+// find the starting and ending position of a given target value.
+// If target is not found in the array, return [-1, -1].
+// You must write an algorithm with O(log n) runtime complexity.
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+func searchRange(nums []int, target int) []int {
+	fo := findFirstOccurance(nums, target)
+	if fo == -1 {
+		return []int{-1, -1}
+	}
+	lo := findLastOccurance(nums, target)
+	return []int{fo, lo}
+
+}
+
+func findFirstOccurance(nums []int, target int) int {
+	ans := -1
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			ans = mid
+			high = mid - 1
+			continue
+		}
+
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+
+	}
+	return ans
+}
+
+func findLastOccurance(nums []int, target int) int {
+	ans := -1
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			ans = mid
+			low = mid + 1
+			continue
+		}
+
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+
+	}
+	return ans
+}
