c06174a8032fc8494b2349e1a733eb1104fb3a64
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-06 11:26:39 +0530

    Rotate Image by 90 deg

--- Array/main.go
+++ Array/main.go
@@ -317,6 +317,31 @@ func maxSubArray(nums []int) int {
 	return maxE
 }
 
+// https://leetcode.com/problems/rotate-image/description/?envType=problem-list-v2&envId=array
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+// DO NOT allocate another 2D matrix and do the rotation.
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+func rotate90(matrix [][]int) {
+	n := len(matrix)
+	// transpose
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	// reverse the rows
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+
+	}
+
+}
+
 func main() {
 	var n int
 	fmt.Println("Enter the size of the array")
