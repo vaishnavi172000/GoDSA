eb7128f0ef146110a1f90cefe8fa5c792f84f9c5
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-07 22:44:17 +0530

    reverse SLL in k groups

--- SLL/main.go
+++ SLL/main.go
@@ -224,6 +224,72 @@ func swapPairs(head *Node) *Node {
 
 }
 
+func reverse(head *Node) *Node {
+	curr := head
+	var prev *Node
+	var next *Node
+	for curr != nil {
+		next = curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+
+	}
+	return prev
+
+}
+
+func FindKthNode(head *Node, k int) *Node {
+	temp := head
+	cnt := 1
+	for cnt != k {
+		if temp == nil {
+			return nil
+		}
+		temp = temp.next
+		cnt++
+	}
+	return temp
+}
+
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+// k is a positive integer and is less than or equal to the length of the linked list.
+// If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [2,1,4,3,5]
+// Example 2:
+
+// Input: head = [1,2,3,4,5], k = 3
+// Output: [3,2,1,4,5]
+func reverseKGroup(head *Node, k int) *Node {
+
+	temp := head
+	Dummy := new(Node)
+	next := head
+	prev := Dummy
+	for temp != nil {
+		kthNode := FindKthNode(temp, k)
+		if kthNode == nil {
+			prev.next = temp
+			break
+		}
+		next = kthNode.next
+		kthNode.next = nil
+		reverse(temp)
+		prev.next = kthNode
+		prev = temp
+		temp = next
+	}
+
+	return Dummy.next
+
+}
+
 func main() {
 	l := new(SLL)
 	l.Create()
