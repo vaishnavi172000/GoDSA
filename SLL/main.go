f1e7f4c81ab2b7f239e0689ad7708129bbe48172
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-07 23:00:36 +0530

    reverse between SLL

--- SLL/main.go
+++ SLL/main.go
@@ -252,6 +252,7 @@ func FindKthNode(head *Node, k int) *Node {
 	return temp
 }
 
+// https://leetcode.com/problems/reverse-nodes-in-k-group/?envType=problem-list-v2&envId=linked-list
 // Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 // k is a positive integer and is less than or equal to the length of the linked list.
 // If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
@@ -286,6 +287,43 @@ func reverseKGroup(head *Node, k int) *Node {
 		temp = next
 	}
 
+	return Dummy.next
+
+}
+
+// https://leetcode.com/problems/reverse-linked-list-ii/description/?envType=problem-list-v2&envId=linked-list
+// Given the head of a singly linked list and two integers left and right where
+// left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.\
+// Example 1:
+// Input: head = [1,2,3,4,5], left = 2, right = 4
+// Output: [1,4,3,2,5]
+// Example 2:
+
+// Input: head = [5], left = 1, right = 1
+// Output: [5]
+func reverseBetween(head *Node, left int, right int) *Node {
+	Dummy := new(Node)
+	Dummy.next = head
+	prev := Dummy
+	temp := head
+	cnt := 1
+	if left == right {
+		return head
+	}
+	for temp != nil && cnt != left {
+		prev = temp
+		temp = temp.next
+		cnt++
+	}
+	leftNode := temp
+	for cnt != right && temp != nil {
+		cnt++
+		temp = temp.next
+	}
+	next := temp.next
+	temp.next = nil
+	prev.next = reverse(leftNode)
+	leftNode.next = next
 	return Dummy.next
 
 }
