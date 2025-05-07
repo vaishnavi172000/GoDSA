5517272e47e88062f0277e32623744760d66104f
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-07 19:18:09 +0530

    Rearrange array

--- Array/main.go
+++ Array/main.go
@@ -488,6 +488,52 @@ func numberOfSubarrays1(nums []int, k int) int {
 func numberOfSubarrays(nums []int, k int) int {
 	return numberOfSubarrays1(nums, k) - numberOfSubarrays1(nums, k-1)
 }
+
+// https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+// You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+// You should return the array of nums such that the the array follows the given conditions:
+
+// Every consecutive pair of integers have opposite signs.
+// For all integers with the same sign, the order in which they were present in nums is preserved.
+// The rearranged array begins with a positive integer.
+// Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+// Example 1:
+
+// Input: nums = [3,1,-2,-5,2,-4]
+// Output: [3,-2,1,-5,2,-4]
+// Explanation:
+// The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+// The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+// Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.
+// Example 2:
+
+// Input: nums = [-1,1]
+// Output: [1,-1]
+// Explanation:
+// 1 is the only positive integer and -1 the only negative integer in nums.
+// So nums is rearranged to [1,-1].
+
+func rearrangeArray(nums []int) []int {
+	postIndex := 0
+	negIndex := 1
+	n := len(nums)
+	result := make([]int, n)
+	for i := 0; i < n; i++ {
+		if nums[i] < 0 {
+			result[negIndex] = nums[i]
+			negIndex += 2
+		} else {
+			result[postIndex] = nums[i]
+			postIndex += 2
+		}
+
+	}
+	return result
+
+}
+
 func main() {
 	var n int
 	fmt.Println("Enter the size of the array")
