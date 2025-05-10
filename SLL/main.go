6a9c314a177095bb5a26428cc50113c8f5bfb3bd
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-10 19:32:46 +0530

    Detect loop in SLL

--- SLL/main.go
+++ SLL/main.go
@@ -328,6 +328,31 @@ func reverseBetween(head *Node, left int, right int) *Node {
 
 }
 
+// https://leetcode.com/problems/linked-list-cycle/
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached
+// again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+func hasCycle(head *Node) bool {
+	if head == nil || head.next == nil {
+		return false
+	}
+	fast := head
+	slow := head
+	for fast != nil && fast.next != nil {
+		fast = fast.next.next
+		slow = slow.next
+		if fast == slow {
+			return true
+		}
+
+	}
+	return false
+
+}
+
 func main() {
 	l := new(SLL)
 	l.Create()
