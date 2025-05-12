d1bf632b38bac55bc224639b6e0414c80efe485e
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-12 18:48:35 +0530

    set zeros in 2d matrix

--- Array/main.go
+++ Array/main.go
@@ -494,6 +494,7 @@ func numberOfSubarrays(nums []int, k int) int {
 	return numberOfSubarrays1(nums, k) - numberOfSubarrays1(nums, k-1)
 }
 
+// 15
 // https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 // You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
 
@@ -539,6 +540,7 @@ func rearrangeArray(nums []int) []int {
 
 }
 
+// 16
 // https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/description
 // Given an array nums, you can perform the following operation any number of times:
 
@@ -603,6 +605,59 @@ func minimumPairRemoval(nums []int) int {
 		nums = mergeArray(nums)
 	}
 	return cnt
+
+}
+
+// Given an m x n integer matrix matrix,
+// if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+
+// Example 1:
+
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+func setZeroes(matrix [][]int) {
+	colZ := 1
+	n := len(matrix)
+	m := len(matrix[0])
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				if j != 0 {
+					matrix[0][j] = 0
+				} else {
+					colZ = 0
+				}
+			}
+
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+	if matrix[0][0] == 0 {
+		for j := 1; j < m; j++ {
+			matrix[0][j] = 0
+		}
+	}
+
+	if colZ == 0 {
+		for i := 0; i < n; i++ {
+			matrix[i][0] = 0
+		}
+	}
 
 }
 
