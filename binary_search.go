cd5888a11c996431c5d7e500f9fbd3b9f8c9e8df
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-17 17:30:19 +0530

    Search in rotated Array

--- binary_search.go
+++ binary_search.go
@@ -349,3 +349,40 @@ func arrangeCoins(n int) int {
 	return int(high)
 
 }
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/?envType=problem-list-v2&envId=binary-search
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function,
+// nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length)
+// such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+//
+//	For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+//
+// Given the array nums after the possible rotation and an integer target,
+// return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+func searchInRotatedArray(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] >= nums[low] {
+			if nums[mid] >= target && nums[low] <= target {
+				high = mid - 1
+			} else {
+				low = mid + 1
+			}
+		} else {
+			if nums[mid] <= target && nums[high] >= target {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+
+}
