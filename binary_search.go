88080ec17af8754aa7d20bd14b1e927083d5809d
vaishnavi <vaishnavi@vaishnavis-MacBook-Air.local>
2025-05-17 19:49:09 +0530

    Search in rotated array with duplicates

--- binary_search.go
+++ binary_search.go
@@ -386,3 +386,54 @@ func searchInRotatedArray(nums []int, target int) int {
 	return -1
 
 }
+
+//There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+// Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length)
+// such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+// For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+// Given the array nums after the rotation and an integer target,
+//  return true if target is in nums, or false if it is not in nums.
+
+// You must decrease the overall operation steps as much as possible.
+
+// Example 1:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+// Output: true
+// Example 2:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+// Output: false
+
+func searchInRotatedArrayWithDuplicates(arr []int, target int) bool {
+	low := 0
+	high := len(arr) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if arr[mid] == target {
+			return true
+		}
+		if arr[low] == arr[mid] && arr[mid] == arr[high] {
+			low++
+			high--
+			continue
+		}
+		if arr[low] <= arr[mid] {
+			if arr[low] <= target && arr[mid] >= target {
+				high = mid - 1
+			} else {
+				low = mid + 1
+			}
+		} else {
+			if arr[high] >= target && arr[mid] <= target {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return false
+
+}
